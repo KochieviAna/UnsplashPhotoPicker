161675232e3ad116edb6b4d8b81217c81255b64f
KochieviAna <anna.kochievi@gmail.com>
2025-05-20 13:26:08 +0400

    Added Xcode project to repository.

---
+++ UnsplashPhotoPicker/UnsplashPhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashPhotoPickerApp.swift
+//  UnsplashPhotoPicker
+//
+//  Created by MacBook on 20.05.25.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashPhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
